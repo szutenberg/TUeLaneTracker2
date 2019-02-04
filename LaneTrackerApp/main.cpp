5e12c17163d2eb9bf36f731595588cf47f5077cc
szutenberg <michal@szutenberg.pl>
2019-02-04 11:26:59 +0100

    Added option to read configuration from yaml file

--- LaneTrackerApp/main.cpp
+++ LaneTrackerApp/main.cpp
@@ -6,22 +6,34 @@
 using namespace std;
 namespace po = boost::program_options;
 
-//Fucntion definitions
+//Function definitions
 unique_ptr<FrameFeeder> createFrameFeeder(FrameSource srcMode,
 		string srcString);
 
+int readConfig(string path, LaneTracker::Config* config);
+
 int main(int argc, char* argv[]) /**
  This is the entry point of the application.
- - Initialises the sigInit handler
+ - Initializes the sigInit handler
  - Creates a stateMachine and spins it until user issues a quit signal through the sigInit handler.
  */
 {
 	int lReturn = 0;
 
 	FrameSource lFrameSource;
-	std::string lSourceStr;
+	string lSourceStr;
+	string lConfigFileName;
 
-	// Prsing command line options
+	unique_ptr<LaneTracker::Config> lPtrConfig;
+	if (lReturn == 0) //create Configuration
+			{
+		lPtrConfig.reset(new LaneTracker::Config);
+		if (lPtrConfig == nullptr) {
+			lReturn = -1;
+		}
+	}
+
+	// Parsing command line options
 	{
 		po::options_description lDesc("Options");
 
@@ -32,7 +44,10 @@ int main(int argc, char* argv[]) /**
 						FrameSource::DIRECTORY), "\t selects frame input mode")
 
 		("Source,s", po::value<string>(&lSourceStr)->default_value("DataSet"),
-				"\t provides source configuration");
+				"\t provides source configuration")
+
+		("Config,c", po::value<string>(&lConfigFileName)->default_value(""),
+				"\t yaml configuration file");
 
 		po::variables_map vMap;
 		po::store(po::parse_command_line(argc, argv, lDesc), vMap);
@@ -54,13 +69,9 @@ int main(int argc, char* argv[]) /**
 
 	} // End parsing command line options
 
-	unique_ptr<LaneTracker::Config> lPtrConfig;
-	if (lReturn == 0) //create Configuration
-			{
-		lPtrConfig.reset(new LaneTracker::Config);
-		if (lPtrConfig == nullptr) {
-			lReturn = -1;
-		}
+	if ((lReturn == 0) && (!lConfigFileName.empty()))
+	{
+		lReturn = readConfig(lConfigFileName, lPtrConfig.get());
 	}
 
 	unique_ptr<FrameFeeder> lPtrFeeder;
@@ -145,7 +156,7 @@ int main(int argc, char* argv[]) /**
 
 	lPtrStateMachine.reset(nullptr);
 	cout << endl << "The program ended with exit code " << lReturn << endl;
-	return (lReturn);
+	return lReturn;
 }
 
 unique_ptr<FrameFeeder> createFrameFeeder(FrameSource srcMode,
@@ -181,3 +192,44 @@ unique_ptr<FrameFeeder> createFrameFeeder(FrameSource srcMode,
 	return lPtrFeeder;
 }
 
+
+int readConfig(std::string path, LaneTracker::Config* cfg)
+{
+	struct stat buf;
+	int statResult = stat(path.c_str(),&buf);
+	if (statResult != 0)
+	{
+		cout << "File " << path << " not found!" << endl;
+		return 2;
+	}
+
+	FileStorage fs(path, FileStorage::READ);
+
+	cfg->lane_avg_width = fs["lane_avg_width"];
+	cfg->lane_std_width = fs["lane_std_width"];
+	cfg->lane_min_width = fs["lane_min_width"];
+	cfg->lane_max_width = fs["lane_max_width"];
+	cfg->lane_marker_width = fs["lane_marker_width"];
+	cfg->cam_res_v = fs["cam_res_v"];
+	cfg->cam_res_h = fs["cam_res_h"];
+	cfg->cam_fx = fs["cam_fx"];
+	cfg->cam_fy = fs["cam_fy"];
+	cfg->cam_cx = fs["cam_cx"];
+	cfg->cam_cy = fs["cam_cy"];
+	cfg->cam_pitch = fs["cam_pitch"];
+	cfg->cam_yaw = fs["cam_yaw"];
+	cfg->cam_height = fs["cam_height"];
+	cfg->cam_lateral_offset = fs["cam_lateral_offset"];
+	cfg->base_line_IBCS = fs["base_line_IBCS"];
+	cfg->purview_line_IBCS = fs["purview_line_IBCS"];
+	cfg->step_lane_filter_cm = fs["step_lane_filter_cm"];
+	cfg->step_vp_filter = fs["step_vp_filter"];
+	cfg->vp_range_ver = fs["vp_range_ver"];
+	cfg->vp_range_hor = fs["vp_range_hor"];
+	cfg->buffer_count = fs["buffer_count"];
+	cfg->display_graphics = ((int)fs["display_graphics"]) != 0;
+	cout << cfg->display_graphics << endl;
+	fs.release();
+
+	return 0;
+}
